02ddd49cbde6423ac16d9d39ac760ba4813c1df5
ominit <86736586+ominit@users.noreply.github.com>
2024-10-11 23:29:47 -0400

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,6 +4,13 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'embark'
 
-config.font = wezterm.font('Iosevka')
+config.font = wezterm.font('IosevkaNerdFont', { weight = 'Medium' })
+config.font_size = 14.0
+
+config.default_prog = { 'nu' }
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_opacity = 0.75
 
 return config
