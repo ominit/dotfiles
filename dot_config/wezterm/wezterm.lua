15bfdab4e507d827e6f7358c3775c4b1956a09ae
ominit <86736586+ominit@users.noreply.github.com>
2024-10-11 23:17:43 -0400

    Update .config/wezterm/wezterm.lua Update packages

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,10 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
--- config.color_scheme_dirs = { '~/.config/wezterm/colors/' }
-
--- config.color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors/" }
-
 config.color_scheme = 'embark'
+
+config.font = wezterm.font('Iosevka')
 
 return config
