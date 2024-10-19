cf30ad5af3c302da92dff6776f4945443cd7a5c1
ominit <86736586+ominit@users.noreply.github.com>
2024-10-18 22:42:32 -0400

    update

--- dot_config/ags/config.js
+++ dot_config/ags/config.js
@@ -1,17 +1,25 @@
-function Bar(monitor =  0) {
-  const label = Widget.Label({
-    label: 'hello'
-  })
+import Gdk from 'gi://Gdk'
+import App from 'resource:///com/github/Aylur/ags/app.js'
 
-  return Widget.Window({
-    monitor,
-    name: `bar${monitor}`,
-    anchor: ['top'],
-    child: label,
-  })
+import { Bar } from './modules/bar/main.js';
+
+const range = (length, start = 1) => Array.from({ length }, (_, i) => i + start);
+function forMonitors(widget) {
+    const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
+    return range(n, 0).map(widget).flat(1);
 }
 
-App.config({ windows: [
-  Bar(0),
-  Bar(1),
-]})
+function forMonitorsAsync(widget) {
+    const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
+    return range(n, 0).forEach((n) => widget(n).catch(print));
+}
+
+const Windows = () => [
+
+];
+
+App.config({
+    windows: Windows().flat(1),
+});
+
+forMonitorsAsync(Bar)
