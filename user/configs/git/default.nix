2e63af5a5d1d11af76cb7433130c7f0798cd0eb0
ominit <86736586+ominit@users.noreply.github.com>
2024-11-02 17:01:41 -0400

    add git-credential-manager

--- user/configs/git/default.nix
+++ user/configs/git/default.nix
@@ -3,7 +3,9 @@
     enable = true;
     userName = "ominit";
     userEmail = "86736586+ominit@users.noreply.github.com";
+    extraConfig.credential.helper = "manager";
+    extraConfig.credential.credentialStore = "cache";
   };
 
-  home.packages = with pkgs; [git gh];
+  home.packages = with pkgs; [git git-credential-manager];
 }
