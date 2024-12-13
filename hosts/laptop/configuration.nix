efbfdeb90ff9ed7a89ebd81d694bcaff3c53842c
ominit <86736586+ominit@users.noreply.github.com>
2024-12-13 22:15:37 +0000

    removed kde/xserver

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -73,17 +73,18 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "colemak";
-  };
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "colemak";
+  # };
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
