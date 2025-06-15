464fd6f54021526815964e0e308cc1a6d72db92d
ominit <86736586+ominit@users.noreply.github.com>
2025-06-14 19:59:27 -0700

    add substituters

--- libEx/builders.nix
+++ libEx/builders.nix
@@ -31,6 +31,14 @@
                 hostPlatform = args.system;
               };
               nix.settings.experimental-features = ["nix-command" "flakes"];
+              nix.settings.substituters = [
+                "https://cache.garnix.io"
+                "https://hyprland.cachix.org"
+              ];
+              nix.settings.trusted-public-keys = [
+                "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+                "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+              ];
               nixpkgs.config.allowUnfree = true;
             }
             ./../modules
