bfec06f3761ef3ef45682d2c556fa11d1617e47f
ominit <86736586+ominit@users.noreply.github.com>
2025-06-17 00:53:31 +0000

    remove eientei host temporarily

--- hosts/default.nix
+++ hosts/default.nix
@@ -7,13 +7,6 @@
     inherit (inputs.self) lib;
     inherit (lib) mkNixosSystem;
   in {
-    eientei = mkNixosSystem {
-      inherit withSystem;
-      hostname = "eientei";
-      system = "x86_64-linux";
-      modules = [./eientei];
-    };
-
     wsl = mkNixosSystem {
       inherit withSystem;
       hostname = "wsl";
