f01826b2b68b9d91d19e8aff730dd37b421b342a
ominit <86736586+ominit@users.noreply.github.com>
2025-07-10 01:34:38 -0700

    makai: mostly fixed gonic

--- hosts/makai/services/caddy.nix
+++ hosts/makai/services/caddy.nix
@@ -7,21 +7,11 @@
         "makai.ominit.io" = {
           extraConfig = ''
             handle /grafana* {
-              reverse_proxy http://${config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port} {
-                header_up Host {http.request.host}
-                header_up X-Real-IP {remote_ip}
-                header_up X-Forwarded-For {remote_ip}
-                header_up X-Forwarded-Proto {http.request.scheme}
-              }
+              reverse_proxy http://${config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}
             }
 
-            handle /gonic* {
-              reverse_proxy http://${config.services.gonic.settings.listen-addr} {
-                header_up Host {http.request.host}
-                header_up X-Real-IP {remote_ip}
-                header_up X-Forwarded-For {remote_ip}
-                header_up X-Forwarded-Proto {http.request.scheme}
-              }
+            handle_path /gonic* {
+              reverse_proxy http://${builtins.head config.services.gonic.settings.listen-addr}
             }
             tls internal
           '';
--- hosts/makai/services/gonic.nix
+++ hosts/makai/services/gonic.nix
@@ -1,18 +1,34 @@
 {...}: {
-  services.gonic = {
-    enable = true;
-    settings = {
-      db-path = "file:///data/services/gonic/gonic.db";
-      music-path = "/data/storage/gonic/music";
-      playlists-path = "/data/storage/gonic/playlists";
-      podcast-path = "/data/storage/gonic/podcasts";
-      cache-path = "/data/services/gonic/cache";
-      listen-addr = "127.0.0.1:10001";
-      scan-interval = 1445;
-      scan-at-start-enabled = true;
-      multi-value-genre = "multi";
-      multi-value-artist = "multi";
-      multi-value-album-artist = "multi";
+  config = {
+    services.gonic = {
+      enable = true;
+      settings = {
+        # db-path = "/data/services/gonic/gonic.db";
+        music-path = "/data/storage/gonic/music";
+        playlists-path = "/data/storage/gonic/playlists";
+        podcast-path = "/data/storage/gonic/podcasts";
+        listen-addr = "127.0.0.1:10001";
+        proxy-prefix = "gonic";
+        scan-interval = 1445;
+        scan-at-start-enabled = true;
+        multi-value-genre = "delim /";
+        multi-value-artist = "delim /";
+        multi-value-album-artist = "delim /";
+      };
     };
+
+    systemd.services.gonic = {
+      serviceConfig = {
+        User = "gonic";
+        Group = "gonic";
+      };
+    };
+
+    users.users.gonic = {
+      isSystemUser = true;
+      group = "gonic";
+    };
+
+    users.groups.gonic = {};
   };
 }
