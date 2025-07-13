2b409c7b5e9cf16c51973141f97c47ddcaad19c8
ominit <86736586+ominit@users.noreply.github.com>
2025-07-12 19:00:31 -0700

    ghostty: add initial configuration and theme files

---
+++ modules/programs/ghostty/default.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkIf mkEnableOption mkOption mkPackageOption;
+
+  pkg = "ghostty";
+in {
+  config = mkIf config.modules.programs."${pkg}".enable {
+    hjem.users."ominit" = {
+      files.".config/ghostty" = {
+        source = ./config;
+        clobber = true;
+      };
+
+      packages = with pkgs; [
+        config.modules.programs."${pkg}".package
+      ];
+    };
+  };
+
+  options.modules.programs."${pkg}" = {
+    enable = mkEnableOption "enable ghostty";
+    package = mkPackageOption pkgs "ghostty" {};
+  };
+}
