4c5d2b83d1c25c8b23b94455f59f0f86d5f226e4
ominit <86736586+ominit@users.noreply.github.com>
2025-10-11 18:27:07 -0400

    add xdg stuff to niri

--- modules/programs/niri/default.nix
+++ modules/programs/niri/default.nix
@@ -20,6 +20,26 @@ in {
       ];
     };
 
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+      ];
+      configPackages = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
+      config = {
+        common = {
+          default = ["wlr" "gtk"];
+          "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+          "org.freedesktop.impl.portal.Screenshot" = ["wlr"];
+          "org.freedesktop.impl.portal.Screencast" = ["wlr"];
+        };
+      };
+    };
+
     programs.niri = {
       enable = true;
       package = config.modules.programs."${pkg}".package;
