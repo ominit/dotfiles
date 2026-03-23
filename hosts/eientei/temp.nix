33d5294043874b1791377f9796b78748cf217dc8
ominit <86736586+ominit@users.noreply.github.com>
2026-03-23 14:04:14 -0400

    add nix-output-monitor

--- hosts/eientei/temp.nix
+++ hosts/eientei/temp.nix
@@ -56,6 +56,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ0KtEKf415TSy1cD+ED/33V7YTtY/I7FZjNR/FNpzXf ominit@wsl"
     ];
     packages = with pkgs; [
+      nix-output-monitor
       anki
       opencode
       fastfetch
