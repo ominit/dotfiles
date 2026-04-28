d959049379f8d12d4844a397ad515a7e3f65b939
ominit <86736586+ominit@users.noreply.github.com>
2026-04-28 12:23:41 -0700

    disable hydra

--- hosts/makai/services/hydra.nix
+++ hosts/makai/services/hydra.nix
@@ -1,35 +1,35 @@
 {...}: {
-  config = {
-    services.hydra = {
-      enable = true;
-      hydraURL = "https://hydra.42nd.net";
-      port = 10008;
-      useSubstitutes = true;
-      notificationSender = "hydra@42nd.net";
-    };
-    nix.buildMachines = [
-      {
-        hostName = "localhost";
-        systems = ["x86_64-linux"];
-        supportedFeatures = [
-          "kvm"
-          "big-parallel"
-          "nixos-test"
-          "benchmark"
-          "ca-derivations"
-          "recursive-nix"
-          "dynamic-derivations"
-        ];
-      }
-    ];
-    fileSystems."/var/lib/postgresql" = {
-      device = "/data/services/hydra/db";
-      options = ["bind"];
-    };
+  # config = {
+  #   services.hydra = {
+  #     enable = true;
+  #     hydraURL = "https://hydra.42nd.net";
+  #     port = 10008;
+  #     useSubstitutes = true;
+  #     notificationSender = "hydra@42nd.net";
+  #   };
+  #   nix.buildMachines = [
+  #     {
+  #       hostName = "localhost";
+  #       systems = ["x86_64-linux"];
+  #       supportedFeatures = [
+  #         "kvm"
+  #         "big-parallel"
+  #         "nixos-test"
+  #         "benchmark"
+  #         "ca-derivations"
+  #         "recursive-nix"
+  #         "dynamic-derivations"
+  #       ];
+  #     }
+  #   ];
+  #   fileSystems."/var/lib/postgresql" = {
+  #     device = "/data/services/hydra/db";
+  #     options = ["bind"];
+  #   };
 
-    fileSystems."/var/lib/hydra" = {
-      device = "/data/services/hydra/data";
-      options = ["bind"];
-    };
-  };
+  #   fileSystems."/var/lib/hydra" = {
+  #     device = "/data/services/hydra/data";
+  #     options = ["bind"];
+  #   };
+  # };
 }
