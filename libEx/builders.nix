b270cd3c31fe3199eedb29a987f92cec5cfb36c8
ominit <86736586+ominit@users.noreply.github.com>
2026-06-09 16:05:10 -0700

    add my cache and remove garnix

--- libEx/builders.nix
+++ libEx/builders.nix
@@ -30,12 +30,14 @@
               nixpkgs.hostPlatform = system;
               nix.settings.experimental-features = ["nix-command" "flakes" "ca-derivations" "dynamic-derivations" "recursive-nix"];
               nix.settings.substituters = [
-                "https://cache.garnix.io"
+                # "https://cache.garnix.io"
                 # "https://hyprland.cachix.org"
+                "https://circus.42nd.net/nix-cache/"
               ];
               nix.settings.trusted-public-keys = [
-                "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+                # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
                 # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+                "circus.42nd.net:JD9qs95wYpbknQHbJRBHs/mW8kIHbWeh5tQhVu3+B3A="
               ];
               nixpkgs.config.allowUnfree = true;
             }
