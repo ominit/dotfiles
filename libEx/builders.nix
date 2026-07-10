e4ab0dae369f0fc2a5915cc20df05ecae30dde79
ominit <86736586+ominit@users.noreply.github.com>
2026-07-10 04:31:33 +0000

    add harmonia as substitutor

--- libEx/builders.nix
+++ libEx/builders.nix
@@ -32,7 +32,7 @@
               nix.settings.substituters = [
                 # "https://cache.garnix.io"
                 # "https://hyprland.cachix.org"
-                "https://circus.42nd.net/nix-cache/"
+                "https://harmonia.42nd.net/"
                 "https://nix-community.cachix.org"
                 "https://attic.xuyh0120.win/lantian"
                 "https://cache.numtide.com"
@@ -40,7 +40,7 @@
               nix.settings.trusted-public-keys = [
                 # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
                 # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-                "circus.42nd.net:JD9qs95wYpbknQHbJRBHs/mW8kIHbWeh5tQhVu3+B3A="
+                "harmonia.42nd.net:JD9qs95wYpbknQHbJRBHs/mW8kIHbWeh5tQhVu3+B3A="
                 "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
                 "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
                 "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
