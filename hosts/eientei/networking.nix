5804d77f1184f3bf61a31441ebfe4f301b7605de
ominit <86736586+ominit@users.noreply.github.com>
2026-08-21 08:26:40 -0500

    update watt and fix timezone

--- hosts/eientei/networking.nix
+++ hosts/eientei/networking.nix
@@ -1,6 +1,7 @@
 {...}: {
   services.openssh.enable = true;
-  services.tzupdate.enable = true;
+  # services.tzupdate.enable = true;
+  time.timeZone = "America/New_York";
   services.netbird.enable = true;
 
   networking.networkmanager.enable = true;
